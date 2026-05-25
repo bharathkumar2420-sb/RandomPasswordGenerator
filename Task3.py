e6ca1abb520cf3cc48e5cd6af7c3fb833fdea86a
Bharath <bharathkumar2024143@gmail.com>
2026-05-25 20:53:06 +0530

    Update Task3.py

--- Task3.py
+++ Task3.py
@@ -3,17 +3,17 @@ import string
 
 try:
     length=int(input("Enter length of the password:"))
+    
+    chara= string.ascii_letters+string.digits+"#$&"
+
+    password=[]
+
+    for i in range(length):
+        password.append(secrets.choice(chara))
+
+    final_password="".join(password)
+
+    print("Generated Password:",final_password)
 
 except ValueError:
     print("ValueError:Input must be a number")
-    
-chara= string.ascii_letters+string.digits+"#$&"
-
-password=[]
-
-for i in range(length):
-    password.append(secrets.choice(chara))
-
-final_password="".join(password)
-
-print("Generated Password:",final_password)
